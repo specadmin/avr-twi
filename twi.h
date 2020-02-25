53cfe842a07f6792d0ed10d623944031e83fd5b5
Anton Rezanov <ton@specadmin.ru>
2020-02-25 19:33:12 +0300

    Code readability improved
    Code readability improved

--- twi.h
+++ twi.h
@@ -1,19 +1,20 @@
 #ifndef TWI_H
 #define TWI_H
-
+//-----------------------------------------------------------------------------
+#include "../avr-misc/avr-misc.h"
 #include <stdint.h>
-
+//-----------------------------------------------------------------------------
 #ifndef TWI_FREQ
 #define TWI_FREQ 100000UL
 #endif
 
-#ifndef TWI_BUFFER_LENGTH
-#define TWI_BUFFER_LENGTH 32
+#ifndef TWI_BUFFER_SIZE
+#define TWI_BUFFER_SIZE 32
 #endif
-
+//-----------------------------------------------------------------------------
 void twi_init();
-void twi_write(uint8_t address, uint8_t* data, uint8_t length, void (*callback)(uint8_t, uint8_t *));
-void twi_read(uint8_t address, uint8_t length, void (*callback)(uint8_t, uint8_t *));
+void twi_write(BYTE address, BYTE* data, uint8_t length, void (*callback)(BYTE, BYTE *));
+void twi_read(BYTE address, BYTE length, void (*callback)(BYTE, BYTE *));
 uint8_t *twi_wait();
-
+//-----------------------------------------------------------------------------
 #endif
